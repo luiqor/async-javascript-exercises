12f221f1949f06791102f18caf5bf0d45a9fa3f7
Diana Vashchilina <vohnenna@gmail.com>
2024-04-25 12:50:47 +0300

    answers for 03-transforming-promises done

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,48 +1,64 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
-function mapPromise(promise, transformer){
+function mapPromise(promise, transformer) {
   return new Promise((resolve, reject) => {
-    /* IMPLEMENT ME!! */
+    promise
+      .then((value) => {
+        try {
+          const transformedValue = transformer(value);
+          resolve(transformedValue);
+        } catch (error) {
+          reject(error);
+        }
+      })
+      .catch((error) => reject(error));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromise(numberPromise){
-  return numberPromise
-    .then(/* IMPLEMENT ME! */);
+function squarePromise(numberPromise) {
+  return numberPromise.then((value) => {
+    const numberValue = typeof value === "string" ? +value : value;
+    if (isNaN(numberValue)) {
+      throw new Error(`Cannot convert '${value}' to a number`);
+    }
+    return Math.pow(numberValue, 2);
+  });
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromiseOrZero(promise){
-  return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+function squarePromiseOrZero(promise) {
+  return squarePromise(promise).catch(() => 0);
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
-function switcheroo(promise){
-  return promise.then(/* IMPLEMENT ME */);
+function switcheroo(promise) {
+  return promise.then(
+    (val) => Promise.reject(val),
+    (err) => Promise.resolve(err)
+  );
 }
 
 /**
