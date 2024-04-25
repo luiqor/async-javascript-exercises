1e90c4e409d71fdbc78bdefe86d12ec4bbf43c06
Diana Vashchilina <vohnenna@gmail.com>
2024-04-25 13:07:46 +0300

    answers for 04-chaining-promises done

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,38 +1,49 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then((value) => {
+        return asyncTransformer(value);
+      })
+      .then(resolve)
+      .catch(reject);
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then(slowAsyncProcess);
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function (id) {
+    return getUserById(id).then((user) => {
+      if (!user) {
+        return undefined;
+      }
+      return getOrganizationById(user.organizationId).then((organization) => {
+        return { ...user, organization };
+      });
+    });
   };
 }
 
