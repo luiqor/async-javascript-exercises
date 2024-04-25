65bef030d2545bd5083bf7e019005d1fad3c7bfe
Diana Vashchilina <vohnenna@gmail.com>
2024-04-25 12:04:14 +0300

    answers for 01-making-promises done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,15 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "comma-dangle": [
-            "error",
-            "always-multiline"
-        ],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "comma-dangle": ["error", "always-multiline"],
+    indent: ["error", 2],
+    semi: ["error", "always"],
+  },
 };
